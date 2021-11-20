81d8c516465946d49486e8b1fb552fa859560422
rapjack <43818577+rapjack@users.noreply.github.com>
2021-11-20 20:55:06 +0530

    all variables and function

--- dependency.js
+++ dependency.js
@@ -2,10 +2,34 @@
 
 const sleep = t => new Promise(s => setTimeout(s, t));
 
+
+let AlertAlgoModal=$('#alertAlgo')
+let IDStartTag=$('#start')
+let IDMazeTag=$('#maze')
+let IDClearTag=$('#clear')
+let IDClearSelectionTag=$('#clearSelection')
+let IDHorizonatalMazeTag=$('#horizontalMaze')
+let IDVerticalMazeTag=$('#verticalMaze')
+let IDRandomMazeTag=$('#randomMaze')
+
+let IDAlgorithmTag=$('#algorithm')
+let IDAlgoDfsTag=$('#classicDfs')
+let IDAlgoBfsTag=$('#classicBfs')
+let IDAlgoAstarTag=$('#aStar')
+
 let maze;
 let asphalt;
+let once=true;
 let sizeX;
 let sizeY;
 let startIndex=undefined;
 let endIndex=undefined;
 let squares = [[]];
+
+/**
+ * value    Algo
+ *  1       BFS;
+ *  2       DFS;
+ *  3       A*;
+ */
+let algoToVisualize;

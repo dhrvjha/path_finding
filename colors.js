97a62df338268b806c83ca678e7a8f4c359d3713
rapjack <43818577+rapjack@users.noreply.github.com>
2021-11-20 20:54:48 +0530

    add color options

---
+++ colors.js
@@ -0,0 +1,18 @@
+
+
+
+
+let SquareDefaultColor='lightgray';
+let SquareSelectedColor='blue';
+let SquarePathColor='yellow'
+let SquareStartColor='green'
+let SquareStartBorderColor='lightgreen'
+let SquareEndColor='red'
+let SquareEndBorderColor='#ffccbb'
+let SquareWallColor='brown';
+let SquareWallBorderColor='#5C4033'
+let SquareSelectedBorderColor='lightBlue'
+let SquareDefaultBorderColor='black'
+let SquarePathBorderColor='#F6BE00'
+
+
